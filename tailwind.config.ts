0afdd5bc9cf8d8edeac53260905d4763eabf7686
Richard <richard232manansala@gmail.com>
2023-08-12 13:37:11 +0800

    extended the colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        onBg: {
+          default: "#1A1A1A",
+          dark: "#0A0A0A",
+          lt: "#2c2c2c",
+          ltr: "#DFDFDF",
+        },
+        primary: {
+          DEFAULT: "#7B2CBF",
+          dark: "#5A189A",
+          lt: "#9D4EDD",
+          ltr: "#E0AAFF",
+        },
+        neutral: {
+          DEFAULT: "#FC",
+          1: "#FC",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
