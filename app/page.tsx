ad1612fd89575e4ee57ed9dbf102ab75bf6a504c
Richard <richard232manansala@gmail.com>
2023-08-12 21:08:03 +0800

    Added media queries and optimized animations

--- app/page.tsx
+++ app/page.tsx
@@ -34,9 +34,9 @@ export default function Home() {
 
         {
           highlightsCircleImageInfo.map((val, index) => (
-            <Image
+              <Image
             style={{animationDelay: `${(index+1) * 0.15}s`}}
-            className={`absolute z-[-10] select-none  highlight__circle-anim translate-x-[-50%] translate-y-[50%] highlight__circle-${val}`}
+            className={` highlight__circle absolute z-[-10] select-none  highlight__circle-anim translate-x-[-50%] translate-y-[50%] highlight__circle-${val}`}
             src="/images/circle.png"
             width={val}
             height={val}
