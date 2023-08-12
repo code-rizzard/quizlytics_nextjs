820e3b727177cbf0e002670e5ac8ebb7c193e965
Richard <richard232manansala@gmail.com>
2023-08-12 13:55:20 +0800

    Added content to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en"
-    className='bg-neutral'
+    className='bg-neutral '
     >
       <body className={inter.className}>
         <Header/>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,21 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <header
+    className="w-full h-[10vh] shadow-md"
+    >
+        <nav
+        className="px-6 py-3 w-full h-full flex flex-row justify-between items-center"
+        >
+            <h2
+            className="text-xl font-extrabold text-primary-dark"
+            >Quizlytics</h2>
+            <button type="button"
+            className="text-primary hover:text-primary-dark sm:hidden"
+            >
+                burger
+            </button>
+        </nav>
+    </header>
   )
 }
 
